f8253704b493c87e1ebb0a6405916a3b0bf6fe18
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-11 01:36:10 +0100

    debug

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ app.use(express.static('build'));
 app.listen(3000);
 
 console.log(`App is listening on port 3000`);
+
+// debug below
+const fs = require('fs');
+fs.readdir('.', (err, files) => {
+  if (err) return console.error(err);
+  
+  files.forEach(file => {
+    console.log(file);
+  });
+});
