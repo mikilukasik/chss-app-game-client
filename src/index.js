b88561321b072dc4e497f6760d34ffa9d583e2b8
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-16 01:59:20 +0100

    ws address

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import App from './components/app';
 import { initWorkers } from './workers';
 import { msgClient } from '../../msg/src/client';
 
-export const gameSocket = msgClient.ws('ws://192.168.1.164:3300/gameSocket');
-export const workersSocket = msgClient.ws('ws://192.168.1.164:3300/workersSocket');
+export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.host}:3300/gameSocket`);
+export const workersSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.host}:3300/workersSocket`);
 
 initWorkers();
 
