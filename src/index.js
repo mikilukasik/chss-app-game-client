6a08f7f6222b0a81e5615fc5e9760e126bd2ea22
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-16 02:12:34 +0100

    hostname for ws

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import App from './components/app';
 import { initWorkers } from './workers';
 import { msgClient } from '../../msg/src/client';
 
-export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.host}:3300/gameSocket`);
-export const workersSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.host}:3300/workersSocket`);
+export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/gameSocket`);
+export const workersSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/workersSocket`);
 
 initWorkers();
 
