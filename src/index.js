4971ec06a479b6f56f01f60e92b9b809d62b64bc
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-17 23:44:09 +0100

    Revert "try to solve ws connection issues"
    This reverts commit f716cfd8c15a14e84ffbd8552eb3e9eca065a8e9.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import App from './components/app';
 import { initWorkers } from './workers';
 import { msgClient } from 'msg/src/client';
 
-const wsAddress = `ws://${typeof window === 'undefined' || window.location.host}`;
-
-export const gameSocket = msgClient.ws(`${wsAddress}/gameSocket`);
-export const workersSocket = msgClient.ws(`${wsAddress}/workersSocket`);
+export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/gameSocket`);
+export const workersSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/workersSocket`);
 
 initWorkers();
 
