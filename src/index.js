f716cfd8c15a14e84ffbd8552eb3e9eca065a8e9
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-17 23:30:37 +0100

    try to solve ws connection issues

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import App from './components/app';
 import { initWorkers } from './workers';
 import { msgClient } from 'msg/src/client';
 
-export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/gameSocket`);
-export const workersSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/workersSocket`);
+const wsAddress = `ws://${typeof window === 'undefined' || window.location.host}`;
+
+export const gameSocket = msgClient.ws(`${wsAddress}/gameSocket`);
+export const workersSocket = msgClient.ws(`${wsAddress}/workersSocket`);
 
 initWorkers();
 
