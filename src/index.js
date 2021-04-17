b35d67a758279b5d32601cf47cc5e1ca4a553745
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-17 22:36:38 +0100

    Revert "use msg via npm"
    This reverts commit f49353458e69e1af58ff0752f6dcd4a9e0799ac0.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style';
 import App from './components/app';
 import { initWorkers } from './workers';
-import { msgClient } from 'msg/src/client';
+import { msgClient } from '../../msg/src/client';
 
 export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/gameSocket`);
 export const workersSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/workersSocket`);
