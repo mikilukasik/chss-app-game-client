ed27780a9743fb5cadc74383790c45153961e508
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-26 00:18:50 +0100

    use logger

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import './style';
 import App from './components/app';
 import { initWorkers } from './workers';
 import { msgClient } from '../msg/src/client';
+import { clientLogger } from '../chss-service-logger';
 
+export const logger = clientLogger({ msgClient });
 export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/gameSocket`);
 
 initWorkers();
-
 export default App;
