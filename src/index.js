f5232d275157fdf113bc01671b7cb805aa211d71
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-30 23:55:16 +0100

    use new logger module

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style';
 import App from './components/app';
 import { initWorkers } from './workers';
 import { msgClient } from '../msg/src/client';
-import { clientLogger } from '../chss-service-logger';
+import { clientLogger } from '../chss-module-logger';
 
 export const logger = clientLogger({ msgClient });
 export const gameSocket = msgClient.ws(`ws://${typeof window === 'undefined' || window.location.hostname}:3300/gameSocket`);
--- src/workers/mainWorker/mainWorkerLogger.js
+++ src/workers/mainWorker/mainWorkerLogger.js
@@ -1,4 +1,4 @@
-import { clientLogger } from "../../../chss-service-logger";
+import { clientLogger } from "../../../chss-module-logger";
 import { msgClient } from '../../../msg/src/client';
 
 export const mainWorkerLogger = clientLogger({ msgClient });
