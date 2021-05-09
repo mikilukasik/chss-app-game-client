7557a0023bf5c46be376ba776ec0def7e8091b86
Miklos Lukasik <miklosbacsi@gmail.com>
2021-05-09 22:23:18 +0100

    game link on scoreBoard

--- src/components/scoreBoard/ScoreBoard.js
+++ src/components/scoreBoard/ScoreBoard.js
@@ -1,17 +1,32 @@
 import { h } from 'preact';
 import { useContext } from 'preact/hooks';
+import { route } from 'preact-router';
 import GameContext from '../../context/GameContext';
+import { setCurrentGameId } from '../../services/gamesService';
 import style from './style.css';
+
+const gameClickHandlers = {};
 
 export const ScoreBoard = () => {
   const { scoreBoardData } = useContext(GameContext);
 	if (!scoreBoardData) return;
 
+	const getGameClickHandler = (id) => {
+		if (gameClickHandlers[id]) return gameClickHandlers[id];
+
+		gameClickHandlers[id] = () => {
+			setCurrentGameId(id);
+			route('/game', true);
+		};
+
+		return gameClickHandlers[id];
+	};
+
 	return (<div>
 		<table><tbody>
-		{scoreBoardData.map(({ name, result, resultScore, moveCount }, index) => resultScore === 1 // draw
-			? <tr><th>{index + 1}.</th><td>{name}</td> <td>played a draw.</td></tr>
-			: <tr><th>{index + 1}.</th><td>{name}</td> <td>{result} in {moveCount} moves.</td></tr>)}
+		{scoreBoardData.map(({ name, result, resultScore, moveCount, gameId }, index) => resultScore === 1 // draw
+			? <tr><th>{index + 1}.</th><td>{name}</td> <td>played a draw.</td><td><a className={style.gameLink} href="#" onClick={getGameClickHandler(gameId)}>game</a></td></tr>
+			: <tr><th>{index + 1}.</th><td>{name}</td> <td>{result} in {moveCount} moves.</td><td><a className={style.gameLink} href="#" onClick={getGameClickHandler(gameId)}>game</a></td></tr>)}
 		</tbody></table>
 	</div>);
 };
