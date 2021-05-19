7c452906597b5cc02f1698d00e032c46248d7d71
Miklos Lukasik <miklosbacsi@gmail.com>
2021-05-19 01:46:54 +0100

    alpha beta pruning all the way

--- src/workers/mainWorker/solveSmallMoveTaskOnSubWorkers.js
+++ src/workers/mainWorker/solveSmallMoveTaskOnSubWorkers.js
@@ -1,6 +1,8 @@
 import { DeepeningTask, oneDeeper, resolveDepth } from '../../../chss-module-engine/src/engine/engine';
 
 export const solveSmallMoveTaskOnSubWorkers = async({ smallMoveTask, getNextAvailableWorker }) => new Promise(async(resolve) => {
+  let aborted = false;
+
   const deepeningTask = new DeepeningTask(smallMoveTask)
   oneDeeper(deepeningTask)
 
@@ -31,15 +33,42 @@ export const solveSmallMoveTaskOnSubWorkers = async({ smallMoveTask, getNextAvai
     resolve(pushAgain)
   };
 
+  const mergeCurrentBests = (receivedCurrentBests) => {
+    const valueToUse = receivedCurrentBests[2];
+    deepeningTask.smallDeepeningTasks.forEach(sdt => {
+      sdt.currentBests[2] = sdt.currentBests[2]
+        ? Math.min(sdt.currentBests[2], valueToUse)
+        : valueToUse;
+    });
+  }
+
   const tasksLength = deepeningTask.smallDeepeningTasks.length - 1;
   while (deepeningTask.smallDeepeningTasks.length > 1) {
+    if (aborted) return;
+    const { worker, release } = await getNextAvailableWorker();
+
+    if (aborted) {
+      release();
+      return;
+    }
+
     const smallDeepeningTask = deepeningTask.smallDeepeningTasks.pop()
     smallDeepeningTask.progress = deepeningTask.progress
 
-    const { worker, release } = await getNextAvailableWorker();
     worker.onmessage = ({ data }) => {
       data.value = data.score;
       res.push(data);
+
+      if (data.score < data.currentBests[1]) {
+        // abort, server already has a better move than this
+        evaluateMove();
+        release();
+        deepeningTask.smallDeepeningTasks.length = 0;
+        aborted = true;
+        return;
+      }
+
+      mergeCurrentBests(data.currentBests);
       release();
       if (res.length === tasksLength) evaluateMove();
     };
